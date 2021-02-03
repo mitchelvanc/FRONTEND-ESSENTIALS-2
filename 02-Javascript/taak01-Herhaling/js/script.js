0a92eead5b2b786c5a8a7750e10e8f90dc60f215
mitchel van cronenburg <mitchel.vancronenburg@student.rocva.nl>
2021-02-03 12:20:14 +0100

    gedaan

--- 02-Javascript/taak01-Herhaling/js/script.js
+++ 02-Javascript/taak01-Herhaling/js/script.js
@@ -1,2 +1,19 @@
 let strNaam;
 let boolNaamKlopt;
+let naamKloptBijna;
+
+strNaam= prompt("hoe heet je?");
+boolNaamKlopt = confirm("heet je echt " + strNaam);
+
+if(boolNaamKlopt == true){
+    alert("welkom " + strNaam)
+}
+else{
+    naamKloptBijna= prompt("voer nogmaals uw naam in!")
+}
+
+    if(naamKloptBijna == true){
+    alert("welkom ")
+}
+    else{("toch welkom! " +strNaam)
+};
