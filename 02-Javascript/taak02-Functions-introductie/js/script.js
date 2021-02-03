5a106225de279e37851486a5ab80d30e6aa42984
mitchel van cronenburg <mitchel.vancronenburg@student.rocva.nl>
2021-02-03 12:35:08 +0100

    gedaan

---
+++ 02-Javascript/taak02-Functions-introductie/js/script.js
@@ -0,0 +1,14 @@
+
+function toonAlertBox(){
+    alert("dit is mijn eerste function")
+};
+   function zelfVerzonnenNaam(){
+        prompt("hoe heet je?")
+   };
+function confirmNaam(){
+    confirm("ik wil door naar de pagina!")
+
+    if(confirm == true){
+        alert("ok!");
+    }
+}
