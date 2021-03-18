e725cabdda2eae4b836fb2430b0d3c218fdf8680
mitchel van cronenburg <mitchel.vancronenburg@student.rocva.nl>
2021-03-18 11:48:23 +0100

    gedaan

--- 02-Javascript/taak09-Arrays-Toevoegen/js/script.js
+++ 02-Javascript/taak09-Arrays-Toevoegen/js/script.js
@@ -10,14 +10,32 @@ topFilms = [
     "One Flew Over the Cuckoo's Nest",
     "Lawrence of Arabia"
 ];
-
+ 
 eigenTopFilms = [];
+ 
 
-//met deze for-loop word de ordered list gevuld met film namen
 for(i = 0; i < topFilms.length; i++){
     document.querySelector(".imdb-film" + (i+1)).innerHTML = topFilms[i];
 }
-
+ 
+var filmnummer = 1; 
+ 
 function kiesFilm(){
-    prompt("nummer 1 film");
+    nummer = prompt("Welke Film staat op plek " + filmnummer);
+        if(nummer >= 0 && nummer <= 10 ){
+            eigenTopFilms.push(topFilms[nummer]); 
+            naam = document.querySelector(".imdb-film" + nummer).innerHTML;
+            let toegevoegd1 = document.querySelector(".eigen-film" + filmnummer);
+            toegevoegd1.innerHTML = naam;
+            console.log(naam);
+            document.querySelector(".imdb-film" + nummer).style.textDecoration = "line-through";;
+            filmnummer++;
+ 
+            if(filmnummer >= 4){
+                document.querySelector('.kiesfilm-button').innerText = 'Klaar';
+            }
+            else{
+                document.querySelector('.kiesfilm-button').innerText = 'Kies Film voor plek ' + filmnummer;
+            }
+        }
 }
